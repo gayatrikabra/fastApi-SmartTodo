8a1fd1040b1adfb6b63b0508472ef892b72abe60
Gayatri Kabra <gayatrikabra@gmail.com>
2026-08-13 15:01:45 +0530

    Fix Railway frontend host and port

--- Frontend/my_app/src/App.js
+++ Frontend/my_app/src/App.js
@@ -25,7 +25,7 @@ function App() {
          
 
           <Route path="/login" element={<Login />} />
-          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+          <Route path="/dashboard" element={<Dashboard />} />
 
 
 
