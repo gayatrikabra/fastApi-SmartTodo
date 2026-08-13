f384ebc57a344405eb17d8e8304de170071ae494
Gayatri Kabra <gayatrikabra@gmail.com>
2026-08-13 13:38:25 +0530

    Fix Railway frontend host and port

--- Backend/main.py
+++ Backend/main.py
@@ -1,36 +1,6 @@
-
-# from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
-
-# from utils.db import Base, engine
-# from Authentication_User.router import auth_routes
-# from Task_User.router import task_routes
-
-# # create tables
-# Base.metadata.create_all(engine)
-
-# app = FastAPI(title="Task Management App")
-# @app.get("/")
-# def root():
-#     return {"message": "Task Management API is running"}
-
-# # CORS FIRST
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["http://localhost:3000",
-#     "https://extended-vanity-choosing.ngrok-free.dev"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-# # routers
-# app.include_router(auth_routes)
-# app.include_router(task_routes)
-
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import text
 
 from utils.db import Base, engine
 from Authentication_User.router import auth_routes
@@ -48,7 +18,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://precious-illumination-production-fce6.up.railway.app/",
+        "https://precious-illumination-production-fce6.up.railway.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
@@ -62,29 +32,38 @@ def root():
     return {"message": "Task Management API is running"}
 
 
+# Database test
+# @app.get("/db-test")
+# def db_test():
+#     try:
+#         with engine.connect() as connection:
+#             result = connection.execute(text("SELECT 1"))
+
+#             return {
+#                 "status": "success",
+#                 "database": "PostgreSQL is connected",
+#                 "result": result.scalar()
+#             }
+
+#     except Exception as e:
+#         return {
+#             "status": "error",
+#             "message": str(e)
+#         }
+
+@app.get("/users-test")
+def users_test(db: Session = Depends(get_db)):
+    users = db.query(User).all()
+
+    return [
+        {
+            "id": user.id,
+            "name": user.name,
+            "email": user.email,
+        }
+        for user in users
+    ]
+
 # Routers
 app.include_router(auth_routes)
 app.include_router(task_routes)
-
-
-
-from sqlalchemy import text
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/db-test")
-def db_test():
-    try:
-        with engine.connect() as connection:
-            result = connection.execute(text("SELECT 1"))
-            return {
-                "status": "success",
-                "database": "PostgreSQL is connected",
-                "result": result.scalar()
-            }
-    except Exception as e:
-        return {
-            "status": "error",
-            "message": str(e)
-        }
